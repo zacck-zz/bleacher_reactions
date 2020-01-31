642796a46bb44cf7904b941174b9bafa89936bfc
Zacck Osiemo <coderv63@gmail.com>
2020-01-31 08:44:07 +0200

    Add test for reactions API

--- test/bleacher_report_web/controllers/reaction_controller_test.exs
+++ test/bleacher_report_web/controllers/reaction_controller_test.exs
@@ -1,5 +1,6 @@
 defmodule BleacherReportWeb.ReactionControllerTest do
   use BleacherReportWeb.ConnCase
+  alias BleacherReport.Cache
 
   describe "Reactions Controller" do
 
@@ -9,6 +10,29 @@ defmodule BleacherReportWeb.ReactionControllerTest do
         |> get(reaction_path(conn, :index))
 
       assert text_response(conn, 200) =~ "Welcome to the bleacher report"
+    end
+
+    test "react/2 should add a reaction to a content's list of reactions", %{conn: conn} do
+      content_id = "some_react_content"
+
+      reaction = %{
+        "type" => "reaction",
+        "action" => "add",
+        "content_id" => content_id,
+        "user_id" => "some_user_id",
+        "reaction_type" => "fire"
+      }
+
+      assert {:error, nil} = Cache.get(content_id)
+
+      conn =
+        conn
+        |> post(reaction_path(conn, :react, reaction))
+
+      assert json_response(conn, 201)
+    end
+
+    test "reaction_counts/2 should fetch reaction counts for a content_id" do
     end
   end
 end
