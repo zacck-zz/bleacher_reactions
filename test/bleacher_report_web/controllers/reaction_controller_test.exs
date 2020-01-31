8e8d48258df0b714c91dd094c6366fce76d07d3b
Zacck Osiemo <coderv63@gmail.com>
2020-01-31 09:46:34 +0200

    Add integration test for retrieving counts that invokes remove

--- test/bleacher_report_web/controllers/reaction_controller_test.exs
+++ test/bleacher_report_web/controllers/reaction_controller_test.exs
@@ -29,10 +29,45 @@ defmodule BleacherReportWeb.ReactionControllerTest do
         conn
         |> post(reaction_path(conn, :react, reaction))
 
-      assert json_response(conn, 201) == %{"content_id" => reaction["user_id"]}
+      assert json_response(conn, 201) == %{"content_id" => content_id, "user_id" => reaction["user_id"]}
     end
 
-    test "reaction_counts/2 should fetch reaction counts for a content_id" do
+    test "count/2 should fetch reaction counts for a content_id", %{conn: conn} do
+      content_id = "some_count_content"
+
+      reaction = %{
+        "type" => "reaction",
+        "action" => "add",
+        "content_id" => content_id,
+        "user_id" => "some_user_id",
+        "reaction_type" => "fire"
+      }
+
+      reaction1 = %{
+        "type" => "reaction",
+        "action" => "remove",
+        "content_id" => content_id,
+        "user_id" => "some_user_id",
+        "reaction_type" => "fire"
+      }
+
+      reaction2 = %{
+        "type" => "reaction",
+        "action" => "add",
+        "content_id" => content_id,
+        "user_id" => "some_remaining_id",
+        "reaction_type" => "fire"
+      }
+
+      conn =
+        conn
+        |> post(reaction_path(conn, :react, reaction))
+        |> post(reaction_path(conn, :react, reaction2))
+        |> post(reaction_path(conn, :react, reaction1))
+        |> get(reaction_path(conn, :count, content_id))
+
+
+      assert json_response(conn, 200)
     end
   end
 end
